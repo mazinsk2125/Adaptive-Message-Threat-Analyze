77ccc775bdd773972604932a7cc455f82a99a93d
Mazin-Shamshad <mskes005@gmail.com>
2025-06-06 15:35:33 +0530

    Render deployment

---
+++ app.py
@@ -0,0 +1,51 @@
+from flask import Flask, request, render_template
+import pickle
+import nltk
+from nltk.corpus import stopwords
+from nltk.stem import PorterStemmer
+from nltk.tokenize import word_tokenize
+import string
+
+# Download nltk resources if you haven't
+nltk.download('punkt')
+nltk.download('stopwords')
+
+app = Flask(__name__)
+
+# Load the saved model and vectorizer
+with open('model.pkl', 'rb') as f:
+    model = pickle.load(f)
+
+with open('vectorizer.pkl', 'rb') as f:
+    vectorizer = pickle.load(f)
+
+stop_words = set(stopwords.words('english'))
+stemmer = PorterStemmer()
+
+def preprocess_text(text):
+    # Lowercase and remove punctuation
+    text = text.lower()
+    text = text.translate(str.maketrans('', '', string.punctuation))
+
+    # Tokenize
+    words = word_tokenize(text)
+
+    # Remove stopwords and stem
+    filtered_words = [stemmer.stem(w) for w in words if w not in stop_words]
+
+    return ' '.join(filtered_words)
+
+@app.route('/', methods=['GET', 'POST'])
+def home():
+    prediction = ""
+    message = ""
+    if request.method == 'POST':
+        message = request.form['message']
+        processed = preprocess_text(message)
+        vector = vectorizer.transform([processed])
+        pred = model.predict(vector)[0]
+        prediction = "Spam" if pred == 1 else "Not Spam"
+    return render_template('index.html', prediction=prediction, message=message)
+
+if __name__ == '__main__':
+    app.run(debug=True)
